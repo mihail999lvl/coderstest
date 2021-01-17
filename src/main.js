166218db925d5b57ef8916ad11e3bb3cd3292ad2
mihail999lvl <landing-r@ya.ru>
2021-01-17 23:51:01 +0400

    commit error code

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import { store } from './store'
+// import { store } from './store'
 import router from './router'
-createApp(App).use(router, store).mount('#app')
+createApp(App).use(router).mount('#app')
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import Vuex from 'vuex'
-import App from './App.vue'
-
-createApp(App).use(Vuex)
-
-export const store = new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {}
-})
+// import { createApp } from 'vue'
+// import Vuex from 'vuex'
+// import App from './App.vue'
+//
+// createApp(App).use(Vuex)
+//
+// export const store = new Vuex.Store({
+//   state: {},
+//   getters: {},
+//   mutations: {},
+//   actions: {}
+// })
